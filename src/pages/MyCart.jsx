9df97f895d4cda50df094ea6b875b2448fc93692
Hannah <hannah1155@naver.com>
2024-02-12 19:06:12 +0900

    장바구니 이미지 배열 첫번째로 보여지기

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,57 +1,49 @@
 import React from 'react';
-import CartItem from '../components/CartItem';
-import { BsFillPlusCircleFill } from 'react-icons/bs';
-import { FaEquals } from 'react-icons/fa';
-import PriceCard from '../components/PriceCard';
-import Button from '../components/ui/Button';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import { AiOutlinePlusSquare, AiOutlineMinusSquare } from 'react-icons/ai';
 import useCart from '../hooks/useCart';
 
-const SHIPPING = 3000;
+const ICON_CLASS =
+  'transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1';
 
-export default function MyCart() {
-  const {
-    cartQuery: { isLoading, data: products },
-  } = useCart();
+export default function CartItem({
+  product,
+  product: { id, images, title, option, quantity, price },
+}) {
+  const { addOrUpdateItem, removeItem } = useCart();
+  const handleMinus = () => {
+    if (quantity < 2) return;
+    addOrUpdateItem.mutate({ ...product, quantity: quantity - 1 });
+  };
+  const handlePlus = () =>
+    addOrUpdateItem.mutate({ ...product, quantity: quantity + 1 });
 
-  console.log(products); 
+  const handleDelete = () => removeItem.mutate(id);
 
-  if (isLoading) return <p>Loading...</p>;
-
-  const hasProducts = products && products.length > 0;
-  const totalPrice =
-    products &&
-    products.reduce(
-      (prev, current) => prev + parseInt(current.price) * current.quantity,
-      0
-  );
-
-  const formattedPrice = totalPrice.toLocaleString(); // 가격을 천 단위 콤마로 포맷팅
+  const formattedPrice = price.toLocaleString(); // 가격을 천 단위 콤마로 포맷팅
+  
+  // 첫 번째 이미지 선택
+  const mainImage = images && images.length > 0 ? images[0] : null;
 
 
   return (
-    <section className='p-2 md:p-8 flex flex-col'>
-      <p className='text-2xl text-center font-bold pb-4 border-b border-gray-300'>
-        내 장바구니
-      </p>
-      {!hasProducts && <div className='flex flex-col items-center justify-center h-96'><p className='font-bold'>장바구니에 상품이 없습니다.</p></div>}
-      {hasProducts && (
-        <>
-          <ul className='border-b border-gray-300 mb-8 p-4 px-2 md:px-8'>
-            {products &&
-              products.map((product) => (
-                <CartItem key={product.id} product={product} />
-              ))}
-          </ul>
-          <div className='flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16'>
-            <PriceCard text='상품 총액' price={formattedPrice}  />
-            <BsFillPlusCircleFill className='shrink-0' />
-            <PriceCard text='배송액' price={SHIPPING.toLocaleString()} />
-            <FaEquals className='shrink-0' />
-            <PriceCard text='총가격' price={(totalPrice + SHIPPING).toLocaleString()} />
-          </div>
-          <Button text='주문하기' />
-        </>
-      )}
-    </section>
+    <li className='flex justify-between my-2 items-center'>
+        {mainImage && (
+          <img className='w-24 md:w-48 rounded-lg' src={mainImage} alt={title} />
+        )}
+      <div className='flex-1 flex justify-between ml-4'>
+        <div className='basis-3/5'>
+          <p className='md:text-lg'>{title}</p>
+          <p className='text-xl font-bold'>{option}</p>
+          <p>₩{formattedPrice}</p>
+        </div>
+        <div className='text-2xl flex items-center'>
+          <AiOutlineMinusSquare className={ICON_CLASS} onClick={handleMinus} />
+          <span>{quantity}</span>
+          <AiOutlinePlusSquare className={ICON_CLASS} onClick={handlePlus} />
+          <RiDeleteBin5Fill className={ICON_CLASS} onClick={handleDelete} />
+        </div>
+      </div>
+    </li>
   );
 }
